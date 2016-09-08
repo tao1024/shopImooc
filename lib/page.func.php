acbd765584fae128db18fdb6282cdc5225df0312
cctvjiatao <cctvjiatao@163.com>
2016-09-08 08:17:40 +0800

    第六天：页码封装第一步

--- lib/page.func.php
+++ lib/page.func.php
@@ -1,8 +1,34 @@
 <?php
-/*
- * Created on 2016-8-23
- *
- * To change the template for this generated file go to
- * Window - Preferences - PHPeclipse - PHP - Code Templates
- */
+require_once 'mysql.func.php';
+connect();
+$sql = "select * from imooc_admin";
+$totalRows = getResultNum($sql);
+echo '总条数：' . $totalRows . '<br/>';
+$pageSize = 2;
+//得到总页数
+$totalPage = ceil($totalRows / $pageSize);
+echo '总页数：' . $totalPage . '<br/>';
+//$page = $_REQUEST['page'] ? (int) $_REQUEST['page'] : 1;
+$page = 3;
+if ($page < 1 || $page == null || !is_numeric($page)) {
+	$page = 1;
+}
+if ($page >= $totalPage){
+	$page = $totalPage;
+}
+
+$offset = ($page -1) * $pageSize;//从第几条开始
+echo 'offset：' . $offset . '<br/>';
+$sql = "select * from imooc_admin limit {$offset},{$pageSize}";
+echo 'sql：' . $sql . '<br/>';
+$rows = fetchAll($sql);
+print_r($rows);
+echo  '<br/>';
+foreach ($rows as $row) {
+	echo "编号：" . $row['id'], "<br/>";
+	echo "管理员的名称:" . $row['username'], "<hr/>";
+}
+//echo showPage($page, $totalPage);
+//echo "<hr/>";
+//echo showPage($page, $totalPage, "cid=5&pid=6");
 ?>
